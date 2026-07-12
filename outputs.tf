de4fa1e72c03736aa085e42520dc93205cf2f34b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,61 +1,57 @@
-output "kusto_iothub_data_connections" {
-  description = "All kusto_iothub_data_connection resources"
-  value       = azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections
-}
 output "kusto_iothub_data_connections_cluster_name" {
-  description = "List of cluster_name values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.cluster_name]
+  description = "Map of cluster_name values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.cluster_name }
 }
 output "kusto_iothub_data_connections_consumer_group" {
-  description = "List of consumer_group values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.consumer_group]
+  description = "Map of consumer_group values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.consumer_group }
 }
 output "kusto_iothub_data_connections_data_format" {
-  description = "List of data_format values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.data_format]
+  description = "Map of data_format values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.data_format }
 }
 output "kusto_iothub_data_connections_database_name" {
-  description = "List of database_name values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.database_name]
+  description = "Map of database_name values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.database_name }
 }
 output "kusto_iothub_data_connections_database_routing_type" {
-  description = "List of database_routing_type values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.database_routing_type]
+  description = "Map of database_routing_type values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.database_routing_type }
 }
 output "kusto_iothub_data_connections_event_system_properties" {
-  description = "List of event_system_properties values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.event_system_properties]
+  description = "Map of event_system_properties values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.event_system_properties }
 }
 output "kusto_iothub_data_connections_iothub_id" {
-  description = "List of iothub_id values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.iothub_id]
+  description = "Map of iothub_id values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.iothub_id }
 }
 output "kusto_iothub_data_connections_location" {
-  description = "List of location values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.location]
+  description = "Map of location values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.location }
 }
 output "kusto_iothub_data_connections_mapping_rule_name" {
-  description = "List of mapping_rule_name values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.mapping_rule_name]
+  description = "Map of mapping_rule_name values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.mapping_rule_name }
 }
 output "kusto_iothub_data_connections_name" {
-  description = "List of name values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.name]
+  description = "Map of name values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.name }
 }
 output "kusto_iothub_data_connections_resource_group_name" {
-  description = "List of resource_group_name values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.resource_group_name]
+  description = "Map of resource_group_name values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.resource_group_name }
 }
 output "kusto_iothub_data_connections_retrieval_start_date" {
-  description = "List of retrieval_start_date values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.retrieval_start_date]
+  description = "Map of retrieval_start_date values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.retrieval_start_date }
 }
 output "kusto_iothub_data_connections_shared_access_policy_name" {
-  description = "List of shared_access_policy_name values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.shared_access_policy_name]
+  description = "Map of shared_access_policy_name values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.shared_access_policy_name }
 }
 output "kusto_iothub_data_connections_table_name" {
-  description = "List of table_name values across all kusto_iothub_data_connections"
-  value       = [for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : v.table_name]
+  description = "Map of table_name values across all kusto_iothub_data_connections, keyed the same as var.kusto_iothub_data_connections"
+  value       = { for k, v in azurerm_kusto_iothub_data_connection.kusto_iothub_data_connections : k => v.table_name }
 }
 
